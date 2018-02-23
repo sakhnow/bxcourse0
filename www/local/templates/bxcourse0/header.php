8e28f42847485e3d28147a89658fec8267677021
Vladimir <sakhnow@gmail.com>
2018-02-23 16:33:29 +0300

    Завершена главная страница

--- www/local/templates/bxcourse0/header.php
+++ www/local/templates/bxcourse0/header.php
@@ -38,6 +38,13 @@ $isMainPage = $APPLICATION->GetCurPage(false) == SITE_DIR;
     Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/vendor/plugins.js');
     Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/vendor/jquery.touchSwipe.js');
     Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/vendor/jquery.ba-throttle-debounce.min.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/main.js');
+  ?>
+  <?php
+    // Слайдер
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/vendor/cookesHelp.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/vendor/bootstrap-switch.min.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/vendor/jquery.carouFredSel-packed.js');
   ?>
 </head>
 <body>
@@ -54,12 +61,20 @@ $isMainPage = $APPLICATION->GetCurPage(false) == SITE_DIR;
       <div class="container">
         <div class="row">
           <div class="col-md-3 col-xs-12">
+            <?php if ($isMainPage): ?>
+            <span class="logo">
+            <?php else: ?>
             <a class="logo" href="#">
-              <div class="image">Intervolga.ru</div>
+            <?php endif ?>
+              <div class="image">Одежда</div>
               <div id="slogan-rand" class="slogan">
                 <noscript>Сайты и реклама в интернете</noscript>
               </div>
+            <?php if ($isMainPage): ?>
+            </span>
+            <?php else: ?>
             </a>
+            <?php endif ?>
           </div>
           <div class="col-sm-6 col-xs-12">
             <div class="row">
@@ -113,13 +128,183 @@ $isMainPage = $APPLICATION->GetCurPage(false) == SITE_DIR;
     </div>
   </nav>
 
+  <?php if ($isMainPage): ?>
+    <div class="slider-responsive">
+      <div class="slider-responsive-panel">
+        <input data-toggle="radio-switch" type="checkbox">
+        <span>Наши лучшие предложения Вам</span>
+      </div>
+      <div class="toggle-height">
+        <div class="slider-responsive-controls">
+          <a class="hidden-xs" href="#prev"></a>
+          <a class="hidden-xs" href="#next"></a>
+        </div>
+        <div class="slider-responsive-inner">
+
+
+          <div class="slider-responsive-inner-item active">
+            <div class="slider-responsive-inner-item-img" style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/img/slider-main/1.jpg')">
+              <div class="slider-responsive-inner-item-img-title">
+                <div class="h2">Одежда для велосипедистов</div>
+                <div>В следующем году наша компания начинает коллекции одежды для велосипедистов. Не пропустите!</div>
+                <a href="#" class="link">Подробнее...</a>
+              </div>
+            </div>
+          </div>
+          <div class="slider-responsive-inner-item">
+            <div class="slider-responsive-inner-item-img" style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/img/slider-main/2.jpg')">
+              <div class="slider-responsive-inner-item-img-title">
+                <div class="h2">Одежда для бега</div>
+                <div>Новая коллекция спортивной одежды для бега</div>
+                <a href="#" class="link">Подробнее...</a>
+              </div>
+            </div>
+          </div>
+          <div class="slider-responsive-inner-item">
+            <div class="slider-responsive-inner-item-img" style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/img/slider-main/3.jpg')">
+              <div class="slider-responsive-inner-item-img-title">
+                <div class="h2">Ценопад! Успейте купить на этой неделе!</div>
+                <div>Наступила осень. Желтеют и опадают листья, навевая уныние и тоску о закончившемся лете. Раскрасьте хмурые будни, спешите на осеннюю акцию Ценопад! Лучшие цены осени, только качественные товары!</div>
+                <a href="#" class="link">Подробнее...</a>
+              </div>
+            </div>
+          </div>
+          <div class="slider-responsive-inner-item">
+            <div class="slider-responsive-inner-item-img" style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/img/slider-main/4.jpg')">
+              <div class="slider-responsive-inner-item-img-title">
+                <div class="h2">Новая коллекция нижнего белья</div>
+                <div>Девочки, спешите! Новое поступление красивейшего нижнего белья. Первым 100 оптовым покупателям - скидка 10%! Отличное качество, известные бренды. В наличии весь размерный ряд.</div>
+              </div>
+            </div>
+          </div>
+          <div class="slider-responsive-inner-item">
+            <div class="slider-responsive-inner-item-img" style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/img/slider-main/5.jpg')">
+              <div class="slider-responsive-inner-item-img-title">
+                <div class="h2">Гимнастические купальники</div>
+                <div>Все для занятия гимнастикой в нашем ассортименте</div>
+                <a href="#" class="link">Подробнее...</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="activities-description-wrap">
+      <div class="activities-description">
+        <div class="container">
+          <div class="activities-inner">
+            <h3>Последние посещенные вами страницы</h3>
+            <ul>
+              <li><a href="#">Мы стали использовать новую ткань</a></li>
+              <li><a href="#">Главная страница</a></li>
+              <li><a href="#">Контакты</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container mbl">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="live-main">
+            <h5><a href="#">Новости</a></h5>
+            <div class="live-pagination">
+              <a class="prev" href="#prev"><i class="fa fa-angle-left"></i></a>
+              <a class="next" href="#next"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <div class="live-list-wrap">
+              <div class="live-list">
+                <div class="live-list-item" id="live-1">
+                  <div class="live-item-body image">
+                    <a class="live-item-img" href=""><img src="<?= SITE_TEMPLATE_PATH ?>/img/live-list/img-1.jpg" alt=""/></a>
+                    <div class="live-item-body-over">
+                      <div class="live-item-description">
+                        <div class="description">Изобретение относится к области получения и применения...</div>
+                      </div>
+                      <div class="live-item-label">
+                        <i class="fa fa fa-twitter"></i>
+                        <a href="#">Новое веяние в одежде - получено прочное водостойкое соединение</a>
+                      </div>
+                      <span class="live-item-data">25 Декабря 2015</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="live-list-item" id="live-2">
+                  <div class="live-item-body image">
+                    <a class="live-item-img" href=""><img src="<?= SITE_TEMPLATE_PATH ?>/img/live-list/img-2.jpg" alt=""/></a>
+                    <div class="live-item-body-over">
+                      <div class="live-item-description">
+                        <div class="description">В следующем году состоится выставка одежды...</div>
+                      </div>
+                      <div class="live-item-label">
+                        <i class="fa fa fa-vk"></i>
+                        <a href="#">Международная выставка одежды Salon De Fashion</a>
+                      </div>
+                      <span class="live-item-data">22 Октября 2015</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="live-list-item" id="live-3">
+                  <div class="live-item-body">
+                    <div class="live-item-body-over">
+                      <div class="live-item-description">
+                        <div class="description">Для пошива нашей одежды мы стали использовать новую
+                          инновационную ткань</div>
+                      </div>
+                      <div class="live-item-label">
+                        <a href="#">Мы стали использовать новую ткань</a>
+                      </div>
+                      <span class="live-item-data">2 Июня 2015</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="live-list-item" id="live-4">
+                  <div class="live-item-body">
+                    <div class="live-item-body-over">
+                      <div class="live-item-description">
+                        <div class="description">Мы расширяем свой размерный ряд. Теперь наша одежда будет
+                          доступна большему количеству людей</div>
+                      </div>
+                      <div class="live-item-label">
+                        <a href="#">Размерный ряд</a>
+                      </div>
+                      <span class="live-item-data">30 Апреля 2015</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="live-list-item" id="live-5">
+                  <div class="live-item-body image">
+                    <a class="live-item-img" href=""><img src="<?= SITE_TEMPLATE_PATH ?>/img/live-list/img-3.jpg" alt=""/></a>
+                    <div class="live-item-body-over">
+                      <div class="live-item-description">
+                        <div class="description">С 20 по 23 февраля состоится...</div>
+                      </div>
+                      <div class="live-item-label">
+                        <i class="fa fa fa-twitter"></i>
+                        <a href="#">Форум производителей одежды в России</a>
+                      </div>
+                      <span class="live-item-data">22 Апреля 2014</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php endif ?>
 
   <div class="container">
+    <?php if (!$isMainPage): ?>
     <ol class="breadcrumb">
       <li><a href="#">Главная</a></li>
       <li><a href="#">Раздел</a></li>
       <li class="active">Детальная страница</li>
     </ol>
-    <h1><?= $APPLICATION->ShowTitle(false) ?></h1>
+    <?php endif ?>
+    <h1><? $APPLICATION->ShowTitle(false) ?></h1>
   </div>
   <div class="container">
